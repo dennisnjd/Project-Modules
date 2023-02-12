01c1425716fdf0905917b5a1fa0dc3d10745b753
Dennis N J <dennisnjd@gmail.com>
2023-02-12 13:50:33 +0530

    forground live location tracking

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+// import Map from '../components/mapview';
+import MapView,{Marker} from 'react-native-maps';
 
-export default function App() {
+
+const App = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [mapRegion , setMapRegion] = useState({
+    latitude: 40.000,
+    longitude: 32.8116347,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+});
+
+
+  const curLoc=async()=>{
+    let acc=0;
+    count=1;
+  let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+    }
+
+    let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true,
+      accuracy: Location.Accuracy.High,});
+    setLocation(location); 
+
+    setMapRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta:  0.0922,
+      longitudeDelta: 0.0421,
+    })
+    console.log(location);
+  }
+
+  useEffect(() => {
+    curLoc();
+    let locationSubscription = Location.watchPositionAsync({
+      accuracy: Location.Accuracy.High,
+      timeInterval: 1000,
+      distanceInterval: 1,
+    }, location => {
+      console.log(location);
+      setMapRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta:  0.0922,
+        longitudeDelta: 0.0421,
+      })
+    });
+    
+  }, []);
+
+  const showmap=()=>{
+    console.log("hi")
+  
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View>
+      <Text style={{marginTop:80}}>longitude:{mapRegion.longitude}</Text>
+      <Text>latitude:{mapRegion.latitude}</Text>
+      <MapView style={styles.map} 
+      // mapType={'hybrid'}
+       region={mapRegion}>
+        <Marker coordinate={mapRegion} title='Marker' />
+      </MapView>
+         </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
+export default App;
+
+
+
+
